82e39b7939c335d069b4a1ede088a9b82be3b7a4
Mimi124 <miriamamoako98@gmail.com>
2023-11-13 12:34:11 +0000

    goals

--- resources/views/layout/frontend_layout/Contents/goal.blade.php
+++ resources/views/layout/frontend_layout/Contents/goal.blade.php
@@ -4,7 +4,7 @@
  @foreach ( $goals as $item)
 
             <div class="col-lg-3 col-md-5 wow fadeInUp" data-wow-delay="0.1s">
-                <img src="{{asset($item->image ) }}" class="img-fluid w-100 rounded"  alt="">
+                <img class="img-fluid rounded" data-wow-delay="0.1s" src="{{asset($item->image ) }}">
             </div>
             <div class="col-lg-6 col-md-7 wow fadeInUp" data-wow-delay="0.3s">
                 <h3 class="text-success">{!! $item->title !!}</h3>
