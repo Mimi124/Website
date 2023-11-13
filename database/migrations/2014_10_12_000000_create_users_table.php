194b7f03ded73941099f7c24482ed7946d9873d4
ekographs <nwaforlorekoate@hotmail.com>
2023-11-13 14:44:12 +0400

    improvements

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
+            $table->string('username', 191)->unique(); // Set a specific key length
             $table->string('phone')->nullable();
             $table->string('photo')->nullable();
             $table->string('gender')->nullable();
             $table->string('address')->nullable();
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique(); // Set a specific key length
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/migrations/2023_11_10_153215_create_our_goals_table.php
+++ database/migrations/2023_11_10_153215_create_our_goals_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('our__goals', function (Blueprint $table) {
+        Schema::create('our_goals', function (Blueprint $table) {
             $table->id();
             $table->text('image');
             $table->string('title')->required();
