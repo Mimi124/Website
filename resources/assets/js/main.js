d2363aa9dd6316161b54546220924c67afeb7ddd
Mimi124 <miriamamoako98@gmail.com>
2024-05-13 14:57:51 +0000

    newsletter added

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -27,17 +27,18 @@
 
 
     // Back to top button
-    $(window).scroll(function () {
+     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+        });
+        $('.back-to-top').click(function () {
+            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+            return false;
+        });
+
 
 
     // Facts counter
@@ -63,15 +64,28 @@
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: false,
+        smartSpeed: 1500,
+        center: true,
+        dots: true,
         loop: true,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
+        margin: 0,
+        nav : true,
+        navText: false,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
     });
 
 
--- vite.config.js
+++ vite.config.js
@@ -6,56 +6,57 @@ export default defineConfig({
         laravel({
             input: [
                 // 'resources/img/favicon.ico',
-                'resources/lib/animate/animate.min.css',
-                'resources/lib/owlcarousel/assets/owl.carousel.min.css',
-                'resources/lib/lightbox/css/lightbox.min.css',
-                'resources/css/bootstrap.min.css',
-                'resources/css/fontawesome-all.min.css',
-                'resources/css/hamburgers.min.css',
-                'resources/css/magnific-popup.css',
-                'resources/css/nice-select.css',
-                'resources/css/slick.css',
-                'resources/css/style.css',
-                'resources/css/themify-icons.css',
-                // 'resources/css/main.css',
-                'resources/js/vendor/swiper/swiper-bundle.min.css',
-                'resources/js/vendor/glightbox/css/glightbox.min.css',
-                'resources/js/vendor/aos/aos.css',
+                'resources/assets/lib/animate/animate.min.css',
+                'resources/assets/lib/owlcarousel/assets/owl.carousel.min.css',
+                'resources/assets/lib/lightbox/css/lightbox.min.css',
+                'resources/assets/css/bootstrap.min.css',
+                'resources/assets/css/style.css',
+                'resources/assets/css/themify-icons.css',
+
+                // 'resources/assets/css/fontawesome-all.min.css',
+                // 'resources/assets/css/hamburgers.min.css',
+                // 'resources/assets/css/magnific-popup.css',
+                // 'resources/assets/css/nice-select.css',
+                // 'resources/assets/css/slick.css',
+                // 'resources/assets/css/main.css',
+                // 'resources/assets/js/vendor/swiper/swiper-bundle.min.css',
+                // 'resources/assets/js/vendor/glightbox/css/glightbox.min.css',
+                // 'resources/assets/js/vendor/aos/aos.css',
 
                 ////////////////JS////////////////////
-                'resources/lib/wow/wow.min.js',
-                'resources/lib/easing/easing.min.js',
-                'resources/lib/waypoints/waypoints.min.js',
-                'resources/lib/owlcarousel/owl.carousel.min.js',
-                'resources/lib/counterup/counterup.min.js',
-                'resources/lib/parallax/parallax.min.js',
-                'resources/lib/isotope/isotope.pkgd.min.js',
-                // 'resources/lib/lightbox/js/lightbox.min.js',
+                'resources/assets/lib/wow/wow.min.js',
+                'resources/assets/lib/easing/easing.min.js',
+                'resources/assets/lib/waypoints/waypoints.min.js',
+                'resources/assets/lib/owlcarousel/owl.carousel.min.js',
+                'resources/assets/lib/counterup/counterup.min.js',
+                'resources/assets/lib/parallax/parallax.min.js',
+                'resources/assets/lib/isotope/isotope.pkgd.min.js',
+                'resources/assets/lib/lightbox/js/lightbox.min.js',
+                'resources/assets/js/main.js',
 
 
-                // 'resources/js/popper.min.js',
-                // 'resources/js/gijgo.min.js',
-                'resources/js/animated.headline.js',
-                'resources/js/jquery.magnific-popup.js',
-                'resources/js/jquery.slicknav.min.js',
-                'resources/js/slick.min.js',
-                'resources/js/jquery.nice-select.min.js',
-                'resources/js/jquery.sticky.js',
-                'resources/js/plugins.js',
-                'resources/js/main.js',
-                // 'resources/js/popper.min.js',
+                // 'resources/assets/js/popper.min.js',
+                // 'resources/assets/js/gijgo.min.js',
+                // 'resources/assets/js/animated.headline.js',
+                // 'resources/assets/js/jquery.magnific-popup.js',
+                // 'resources/assets/js/jquery.slicknav.min.js',
+                // 'resources/assets/js/slick.min.js',
+                // 'resources/assets/js/jquery.nice-select.min.js',
+                // 'resources/assets/js/jquery.sticky.js',
+                // 'resources/assets/js/plugins.js',
+                // 'resources/assets/js/popper.min.js',
 
-                'resources/js/vendor/aos/aos.js',
-                'resources/js/vendor/glightbox/js/glightbox.min.js',
-                // 'resources/vendor/isotope-layout/isotope.pkgd.min.js',
-                'resources/js/vendor/swiper/swiper-bundle.min.js',
-
-
-
+                // 'resources/assets/js/vendor/aos/aos.js',
+                // 'resources/assets/js/vendor/glightbox/js/glightbox.min.js',
+                // // 'resources/vendor/isotope-layout/isotope.pkgd.min.js',
+                // 'resources/assets/js/vendor/swiper/swiper-bundle.min.js',
 
 
 
-                'resources/css/app.css',
+
+
+
+                // 'resources/css/app.css',
                 //  'resources/js/app.js',
             ],
             refresh: true,
@@ -63,7 +64,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '$': 'jQuery',
+            // '$': 'jQuery',
             // '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         },
     },
